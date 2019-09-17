2a0c6a10b9feb2a7bacf861a3a02eea6d6477196
Lukasz <lukasz.kowalik41@gmail.com>
2019-09-17 18:39:51 +0200

    added attributes into Grade class

--- Database/Grade/Grade.cs
+++ Database/Grade/Grade.cs
@@ -1,14 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
 using System.Windows.Data;
+using static Database.DataBase;
+using static Database.ShowGrades;
 
 namespace Database
 {
+
+  [DBTab]
     public sealed class Grade
     {
-        public double gradeValue { get;}
+        [DBColl(Title="Nr Indeksu Studenta",ForeignKey= "[dbo].[Students]([StudentNo])")]
+        public int StudentNo { get; set; }
+        [DBColl(Title ="Ocena")]
+        public double Value { get; set; }
+        [DBColl(Title ="Data wystawienia")]
+        public DateTime Date { get; set; }
+        public Grade()
+        {
+
+        }
         public Grade(double grade)
         {
-            gradeValue = grade;
+           Value = grade;
+        }
+        public Grade(int studentNo,double value, DateTime date)
+        {
+            StudentNo = studentNo;
+            Value = value;
+            Date = date;
         }
     }
 }
